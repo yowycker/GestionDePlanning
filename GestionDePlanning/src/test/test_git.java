ed04f4b0641acd6e50f2d9ed94258e71b74c4c27
Yoann <Yoann@pc-Yoann>
2015-03-10 01:05:19 +0100

    Revert "Revert "test commit and push""
    This reverts commit 8bd56acb030367dbf3217949d3673bf0025d898b.

---
+++ GestionDePlanning/src/test/test_git.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test_git {
+	public test_git(String test){
+		System.out.println("hello " + test);
+	}
+	public static void main(String[] args){
+		new test_git("Pierre");
+	}
+}
