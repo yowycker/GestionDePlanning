d7e3d2e670d9eba63a89290fe81ef192a255d67e
Pierre <Pierre@Pierre-PC>
2015-05-14 17:33:24 +0200

    Test

---
+++ GestionDePlanning/src/test/testDayFormationSeances.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class testDayFormationSeances extends TestCase{
+
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ GestionDePlanning/src/test/testFormation.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import Model.CalendarObject.Formation;
+
+public class testFormation extends TestCase{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
