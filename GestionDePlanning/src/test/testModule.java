7376f7be0fae9822b55d450e6e9ebd849e247bb0
Pierre <Pierre@Pierre-PC>
2015-05-16 16:41:37 +0200

    tests unitaires Module ok

--- GestionDePlanning/src/test/testModule.java
+++ GestionDePlanning/src/test/testModule.java
@@ -12,6 +12,15 @@ import Model.CalendarObject.Module;
 
 public class testModule extends TestCase{
 
+	public void testinstantiateISeance(){
+		Module n = new Module("Anglais", "AN", Color.white, 10 );
+		assertTrue("", n.getISeance() == 0);
+		n.instantiateISeance();
+		assertTrue("", n.getISeance() == 1);
+		n.dropInstantiateISeance();
+		assertTrue("", n.getISeance() == 0);
+	}
+	
 	public void testgetName() {
 		Module n = new Module("Anglais", "AN", Color.white, 10 );
 		assertTrue("Le module doit avoir un nom", n.getName() == "Anglais" );
@@ -22,5 +31,34 @@ public class testModule extends TestCase{
 		assertTrue("Le module doit avoir une abbreviation", n.getAbbreviation() == "AN" );
 	}
 
+	public void testgetColor() {
+		Module n = new Module("Anglais", "AN", Color.white, 10 );
+		assertTrue("Le module doit avoir une couleur", n.getColor() == Color.white );
+	}
+	
+	/*public void testgetTeacher() {
+		Module n = new Module("Anglais", "AN", Color.white, "Champroux", 10 );
+		assertTrue("Le module doit avoir un enseignant", n.getTeacher() == "Champroux" );
+	}*/
+	
+	public void testgetMaxSeances() {
+		Module n = new Module("Anglais", "AN", Color.white, 10 );
+		assertTrue("Le module doit avoir un nombre de seance max", n.getMaxSeances() == 10 );
+	}
+	
+	public void testEqualsName(){
+		Module n = new Module("Anglais", "AN", Color.white, 10 );
+		assertFalse("Le module doit avoir un nom", n.getName() == "" );
+	}
+	
+	public void testEqualsAbbreviation(){
+		Module n = new Module("Anglais", "AN", Color.white, 10 );
+		assertFalse("Le module doit avoir une abbreviation", n.getAbbreviation() == "" );
+	}
+	
+	public void testEqualsColor(){
+		Module n = new Module("Anglais", "AN", Color.white, 10 );
+		assertFalse("Le module doit avoir une couleur", n.getColor() == Color.red );
+	}
 	
 }
